f24564bedca205904c994d1d91510bd10413bdd1
Pepijn Bruienne <bruienne@gmail.com>
2013-05-28 13:56:19 -0400

    Cleaned up option parsing and CLI help text. Some minor formatting changes to follow PEP8.

--- manifest_bundler/manifest_bundler.py
+++ manifest_bundler/manifest_bundler.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 import sys
+
 sys.path.append("/usr/local/munki/munkilib")
 import munkicommon
 import FoundationPlist
@@ -12,20 +13,22 @@ from optparse import OptionParser
 import requests
 
 # Setup option parsing
-parser = OptionParser("Usage: %prog [/path/to/manifest] [-o | --optional-software] [-n | --nested-manifests] [-u | --upload http://mymunkiserver/{UNIT}/packages/batch ]")
+parser = OptionParser(
+    'Usage: %prog [/path/to/manifest] [-o | --optional-software] [-n | --nested-manifests] \
+    [-u | --upload http://mymunkiserver/{UNIT}/packages/batch ]')
 
 # Parse our program arguments
 parser.add_option("-o", "--optional-software",
-                    action="store_true", dest="optional", default=False,
-                    help="Also process the optional software in the manifest")
+                  action="store_true", dest="optional", default=False,
+                  help="Also process the optional software in the manifest")
 # Process nested manifests
 parser.add_option("-n", "--nested-manifests",
-                    action="store_true", dest="nested", default=False,
-                    help="Process nested manifests as well")
+                  action="store_true", dest="nested", default=False,
+                  help="Process nested manifests as well")
 
 parser.add_option("-u", "--upload",
-                    action="store", dest="url",
-                    help="Upload to Munkiserver, requires valid URL for MS upload")
+                  action="store", dest="url",
+                  help="Upload to Munkiserver, requires valid URL for MS upload")
 
 (options, args) = parser.parse_args()
 
@@ -43,7 +46,8 @@ else:
     manifestpath = sys.argv[1]
 
     if processoptional and processnested and url:
-        print "Processing manifest including nested manifests and optional software from " + manifestpath + " and uploading to " + url + "\n"
+        print "Processing manifest including nested manifests and optional software from " + manifestpath + \
+              ' and uploading to ' + url + "\n"
     elif processoptional and processnested:
         print "Processing manifest including nested manifests and optional software from " + manifestpath + "\n"
     elif processoptional and url:
@@ -75,23 +79,25 @@ pkgsinfodir = os.path.join(basedir, "pkgsinfo")
 pkgsdir = os.path.join(basedir, "pkgs")
 managedinstalls = ""
 
+
 def uploadToMunkiserver(pkg, pkginfo):
     """docstring for uploadToMunkiserver"""
-    
+
     s = requests.Session()
 
     # s.params = {'autoconfig':'true'}
     # url = 'http://localhost:3000/default/packages/batch'
 
     files = {'package_file': open(pkg, 'rb'), 'pkginfo_file': open(pkginfo, 'rb')}
-    
+
     print "Uploading " + os.path.basename(pkginfo) + " to " + url
-    
+
     result = s.post(url, files=files)
     # print result.text
     # print result.status_code
-        
+
     # pass
+
 
 def processManifest(manifestpath, nested=False):
     """docstring for processManifest"""
@@ -108,17 +114,19 @@ def processManifest(manifestpath, nested=False):
         if processnested:
             nestedmanifests = plist.get("included_manifests")
             for thismanifest in nestedmanifests:
-                managedinstalls = managedinstalls + processManifest(os.path.join(manifestdir, thismanifest), nested=True)
+                managedinstalls = managedinstalls + processManifest(os.path.join(manifestdir, thismanifest),
+                                                                    nested=True)
 
     else:
         print "Not a file: " + options.manifestpath
     return managedinstalls
 
+
 # Takes a pkginfo name and a path and looks up the corresponding install_item_location (i.e. the installer)
-def findInstallerItem(thispkginfo,thispath):
+def findInstallerItem(thispkginfo, thispath):
     # Basic test of file suitability, is it a file?
     if os.path.isfile(thispath + "/" + thispkginfo):
-        
+
         # It's a file so parse it as a plist and retrieve the installer_item_location value
         #  Also retrieve the installer_type to catch 'nogpk' installs
         #  TODO: Should test for a valid plist as well
@@ -127,7 +135,7 @@ def findInstallerItem(thispkginfo,thispath):
         installer_type = thisplist.get("installer_type")
 
         # Create the full paths to installer_item_location and the pkginfo
-        
+
         # We must catch a missing install_item_location key, usually if the pkginfo describes a 'nopkg' installation
         if installer_type != 'nopkg':
             pkg = pkgsdir + "/" + thisitemlocation
@@ -136,9 +144,9 @@ def findInstallerItem(thispkginfo,thispath):
             #  the filetype of the supposed dmg so we use this to fake it out. More elegant solution needed.
             print 'The installer_type is nopkg, faking out Munkiserver'
             pkg = os.path.join(thispath, thispkginfo)
-        
+
         pkginfo = os.path.join(thispath, thispkginfo)
-        
+
         # Verify that the pkginfo is a file
         if os.path.isfile(pkginfo):
             # Do stuff. For now we just print the result.
@@ -161,14 +169,14 @@ def findInstallerItem(thispkginfo,thispath):
 
     if url:
         uploadToMunkiserver(pkg, pkginfo)
-    
-    # return pkg, pkginfo
-    
-def getHighestVersion(thisapp):
 
+        # return pkg, pkginfo
+
+
+def getHighestVersion(thisapp):
     # Initialize version to keep a running tally of the highest version as we iterate
-    version="0.0"
-    
+    version = "0.0"
+
     # Iterate over thisapp and extract the app name and version separately
     for i in [os.path.splitext(x)[0] for x in thisapp]:
         currentapp = munkicommon.nameAndVersion(i)[0]
@@ -176,29 +184,29 @@ def getHighestVersion(thisapp):
 
         # Check whether the current iteration's version is higher than our running tally
         if parse_version(thisversion) > parse_version(version):
-
             # It's higher, so update the version now
             version = thisversion
             pkginfo = i + ".plist"
-            
+
     return pkginfo
-        
+
+
 def bundleItems(pkgsinfodir, thismanagedinstalls):
     # Iterate over the given directory and get all filenames
     for (path, dirs, files) in os.walk(pkgsinfodir):
-    
+
         # Now iterate over the managedinstalls items
         for item in thismanagedinstalls:
             # Create an empty list in case we encounter multiple versions of an app
-            thisapp=[]
+            thisapp = []
             explicitversion = ""
-        
+
             # Catch managed_installs items with explicit versions so we can honor them
             if re.search('\-[0-9]', item):
                 itemandversion = item.split('-')
                 item = itemandversion[0]
                 explicitversion = itemandversion[1]
-        
+
             # Now we iterate over the files found in the directory with os.walk
             for thisfile in files:
 
@@ -206,23 +214,28 @@ def bundleItems(pkgsinfodir, thismanagedinstalls):
                 # If it matches append the filename to the thisapp list
                 if thisfile.startswith(item + "-"):
                     thisapp.append(thisfile)
-                
+
             # After iterating and finding one or more versions of the current thismanagedinstalls item
             #  we move on to figuring out the newest version of the app - we don't want older ones.
             if len(thisapp) > 1:
                 pkginfo = getHighestVersion(thisapp)
-                findInstallerItem(pkginfo,path)
-            
+                findInstallerItem(pkginfo, path)
+
             # If there's only one version found just run findInstallerItem
             elif len(thisapp) == 1 and not explicitversion:
                 pkginfo = thisapp[0]
-                findInstallerItem(pkginfo,path)
+                findInstallerItem(pkginfo, path)
 
+
+# Run it
 def main():
-    
-    # Run it
+    """
+    We call processManifest and store the results in itemstobundle which is then passed to
+    bundleItems to do the final processing.
+    """
     itemstobundle = processManifest(manifestpath)
     bundleItems(pkgsinfodir, itemstobundle)
-    
+
+
 if __name__ == '__main__':
     main()
